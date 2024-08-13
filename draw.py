ffaf822c828a2c5da681bcd77aaf460089eae5a4
Patricia Duarte de Lima Machado <40029758+pdlmachado@users.noreply.github.com>
2024-08-12 21:56:04 -0300

    Update draw.py

--- draw.py
+++ draw.py
@@ -28,7 +28,7 @@ def drawgv_TM(dtm, layoutid, name):
         #print(s,i,t,j,m)
         #SD1.add_edge(s,t,label=f"{i} {chr(0x2192)} {j}, {m}")
         SD1.edge(s,t,label=f"{i} {chr(0x2192)} {j}, {m}")
-  SD1.view()
+  #SD1.view()
   return SD1
 
 # Desenha uma DTM. O parâmetro dtm é uma TM determinística no formato de automata-lib
