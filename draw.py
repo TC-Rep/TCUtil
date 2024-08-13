0faf61a08fe929546d77d2e2a6072d2cab0b80c2
Patricia Duarte de Lima Machado <40029758+pdlmachado@users.noreply.github.com>
2024-08-12 21:39:09 -0300

    Update draw.py

--- draw.py
+++ draw.py
@@ -1,5 +1,35 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import graphviz
+
+# Desenha uma TM no formato GraphViz
+def drawgv_TM(dtm, layoutid, name):
+  #blank = chr(0x2294)
+  blank = '_'
+  SD1 = graphviz.Digraph(name=name, 
+                       filename=name+ '.gv', 
+                       format='png', 
+                       engine=layoutid)
+  SD1.attr(rankdir='LR')
+  for s in dtm.states:
+    if s in dtm.final_states:
+      SD1.node(s, shape='doublecircle', style='filled', fillcolor='lightgreen')
+    elif s == dtm.initial_state:
+      SD1.node(s, shape='circle', style='filled', fillcolor='yellow')
+    else:
+      SD1.node(s, shape='circle', style='filled', fillcolor='lightgray')
+  for s,s_dict in dtm.transitions.items():
+      for i,t_tuple in s_dict.items():
+        t,j,m = t_tuple
+        if i == dtm.blank_symbol:
+          i = blank
+        if j == dtm.blank_symbol:
+          j= blank
+        #print(s,i,t,j,m)
+        #SD1.add_edge(s,t,label=f"{i} {chr(0x2192)} {j}, {m}")
+        SD1.edge(s,t,label=f"{i} {chr(0x2192)} {j}, {m}")
+  SD1.view()
+  return SD1
 
 # Desenha uma DTM. O parâmetro dtm é uma TM determinística no formato de automata-lib
 def draw_TM(dtm,layoutid):
