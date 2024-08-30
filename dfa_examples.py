342fc742c3771823203616a32e8fa0a41c2bb5f6
Patricia Duarte de Lima Machado <40029758+pdlmachado@users.noreply.github.com>
2024-08-30 16:32:24 -0300

    Update dfa_examples.py

--- dfa_examples.py
+++ dfa_examples.py
@@ -49,3 +49,16 @@ D4 = DFA(
     initial_state='q0',
     final_states={'q0'}
 )
+
+# Reconhece a linguagem vazia
+D5 = DFA(
+    states={'q0', 'q1', 'q2'},
+    input_symbols={'a', 'b'},
+    transitions={
+        'q0': {'a': 'q1', 'b': 'q0'},
+        'q1': {'a': 'q1', 'b': 'q1'},
+        'q2': {'a': 'q2', 'b': 'q2'},
+    },
+    initial_state='q0',
+    final_states={'q2'}
+)
