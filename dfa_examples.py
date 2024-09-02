dff430c3b12cf242ebcedd984e8d0b985fd350f9
Patricia Duarte de Lima Machado <40029758+pdlmachado@users.noreply.github.com>
2024-09-02 19:59:54 -0300

    Update dfa_examples.py

--- dfa_examples.py
+++ dfa_examples.py
@@ -56,9 +56,9 @@ D5 = DFA(
     input_symbols={'a', 'b'},
     transitions={
         'q0': {'a': 'q1', 'b': 'q0'},
-        'q1': {'a': 'q1', 'b': 'q1'},
+        'q1': {'a': 'q1', 'b': 'q2'},
         'q2': {'a': 'q2', 'b': 'q2'},
     },
     initial_state='q0',
-    final_states={'q2'}
+    final_states={}
 )
