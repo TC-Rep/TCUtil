adcf8ce0a9f11a68cf8a44dee4079ff8bf04eb0d
Patricia Duarte de Lima Machado <40029758+pdlmachado@users.noreply.github.com>
2026-07-20 11:43:42 -0300

    Update dfa_examples.py

--- dfa_examples.py
+++ dfa_examples.py
@@ -99,3 +99,16 @@ D8 = DFA(
     final_states={'q2'}
 )
 
+# Reconhece Todas as Cadeias (Sigma*)
+D9 = DFA(
+    states={"q0"},
+    input_symbols={"0", "1"},
+    transitions={
+        "q0": {
+            "0": "q0",
+            "1": "q0",
+        }
+    },
+    initial_state="q0",
+    final_states={"q0"},
+)
